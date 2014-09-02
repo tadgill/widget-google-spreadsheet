2e768e662385969f633472d30b107dc92dfd1771
Stuart Lees <stuartlees@gmail.com>
2014-09-02 16:29:51 -0400

    Updating gulp build and testing processes
    - Keeping in line with gulp tasks that Widget Financial Table has
    - "json-move" and "json-combine" tasks added for i18n translation.json files
    - New build tasks "css", "fonts", "i18n" added to sequence

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,35 @@
   "use strict";
 
   var gulp = require("gulp");
+  var fs = require("fs");
+  var es = require("event-stream");
   var gutil = require("gulp-util");
   var rimraf = require("gulp-rimraf");
   var concat = require("gulp-concat");
   var bump = require("gulp-bump");
-  var html2js = require("gulp-html2js");
   var jshint = require("gulp-jshint");
+  var jsoncombine = require("gulp-jsoncombine");
+  var minifyCSS = require("gulp-minify-css");
+  var usemin = require("gulp-usemin");
   var uglify = require("gulp-uglify");
   var runSequence = require("run-sequence");
   var path = require("path");
   var rename = require("gulp-rename");
   var factory = require("widget-tester").gulpTaskFactory;
 
-  var jsFiles = [
+  var appJSFiles = [
     "src/**/*.js",
-    "test/**/*.js",
     "!./src/components/**/*"
   ];
+
+  var cssFiles = [
+    "src/css/**/*.css"
+  ];
+
+  var languages = fs.readdirSync("src/locales")
+    .filter(function(file) {
+      return fs.statSync(path.join("src/locales", file)).isDirectory();
+    });
 
   gulp.task("clean-dist", function () {
     return gulp.src("dist", {read: false})
@@ -50,7 +62,7 @@
   });
 
   gulp.task("lint", function() {
-    return gulp.src(jsFiles)
+    return gulp.src(appJSFiles)
       .pipe(jshint())
       .pipe(jshint.reporter("jshint-stylish"));
     // .pipe(jshint.reporter("fail"));
@@ -58,16 +70,70 @@
 
   gulp.task("html", ["lint"], function () {
     return gulp.src(['./src/*.html'])
+      .pipe(usemin({
+        js: [uglify({
+          mangle:true,
+          outSourceMap: false // source map generation doesn't seem to function correctly
+        })]
+      }))
       .pipe(gulp.dest("dist/"));
   });
 
-  gulp.task("i18n", function() {
-    return gulp.src("src/locales/**/*.json")
-      .pipe(gulp.dest("dist/locales"));
+  gulp.task("css", function () {
+    return gulp.src(cssFiles)
+      .pipe(minifyCSS({keepBreaks:true}))
+      .pipe(concat("all.min.css"))
+      .pipe(gulp.dest("dist/css"));
+  });
+
+  gulp.task("fonts", function() {
+    return gulp.src("src/components/common-style/dist/fonts/**/*")
+      .pipe(gulp.dest("dist/fonts"));
+  });
+
+  gulp.task("json-move", function() {
+    // in case some files have the same name
+    var index = 0;
+    var tasks = languages.map(function(folder) {
+      return gulp.src([path.join("src/locales", folder, "*.json"),
+        path.join("src/components/*/dist/locales", folder, "*.json")])
+        .pipe(rename(function (path) {
+          path.dirname = "";
+          path.basename += index++;
+        }))
+        .pipe(gulp.dest(path.join("tmp/locales", folder)));
+    });
+    return es.concat.apply(null, tasks);
+  });
+
+  gulp.task("json-combine", ["json-move"], function() {
+    var tasks = languages.map(function(folder) {
+      return gulp.src([path.join("tmp/locales", folder, "*.json")])
+        .pipe(jsoncombine("translation.json",function(data) {
+          var jsonString,
+            newData = {};
+
+          for (var filename in data) {
+            var fileObject = data[filename];
+            for (var attrname in fileObject) {
+              newData[attrname] = fileObject[attrname];
+            }
+          }
+
+          jsonString = JSON.stringify(newData, null, 2);
+          return new Buffer(jsonString);
+        }))
+        .pipe(gulp.dest(path.join("dist/locales/", folder)));
+    });
+    return es.concat.apply(null, tasks);
+  });
+
+  gulp.task("i18n", function(cb) {
+    runSequence("json-move", "json-combine", cb);
   });
 
   gulp.task('build', function (cb) {
-    runSequence(["clean", "config"], ["html", "i18n"], cb);
+    runSequence(["clean", "config"], ["html", "css", "fonts", "i18n"], cb);
   });
 
   gulp.task("html:e2e", factory.htmlE2E());
@@ -91,7 +157,7 @@
     runSequence("test:e2e:settings", "test:metrics", cb);
   });
 
-  gulp.task("default", ["build"]);
+  gulp.task("default", ["test"]);
 
 
 })();
