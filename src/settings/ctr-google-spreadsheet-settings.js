731ddcce8866d86f32eefb22cbab64902bd64da3
Stuart Lees <stuartlees@gmail.com>
2014-09-08 17:14:17 -0400

    Base setup for column setting component population
    - Adding "columnsService" factory which will be responsible for querying google visualization api to obtain and return configured spreadsheet worksheet columns data
    - spreadsheet settings controller now watching for changes to sheet property of additionalParams.spreadsheet object so logic can be put in place for updating/refreshing column setting component
    - settings HTML now importing google jsapi and loading the visualization api
    - spreadsheet settings app now adding dependency on visualization module from widget-settings-ui-core

--- src/settings/ctr-google-spreadsheet-settings.js
+++ src/settings/ctr-google-spreadsheet-settings.js
@@ -1,8 +1,17 @@
 angular.module("risevision.widget.googleSpreadsheet.settings")
-  .controller("spreadsheetSettingsController", ["$scope",
-    function ($scope) {
+  .controller("spreadsheetSettingsController", ["$scope", "$log",
+    function ($scope, $log) {
 
       $scope.sheetColumns = [];
+
+      $scope.$watch("settings.additionalParams.spreadsheet.sheet", function (newSheetUrl, oldSheetUrl) {
+        if (typeof newSheetUrl !== "undefined") {
+          if (newSheetUrl !== oldSheetUrl) {
+            $log.info("worksheet change: ", newSheetUrl);
+            //TODO: logic for using columns settings service to eventually update/refresh column setting component
+          }
+        }
+      });
 
     }])
   .value("defaultSettings", {
