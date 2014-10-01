8bd01a8206a70851f79cf4f1e726aaba51cb4257
Stuart Lees <stuartlees@gmail.com>
2014-10-01 17:15:09 -0400

    Retrieving visualization table data
    - `additionalParams` function updated to create visualization instance, prefs instance, store the spreadsheet data passed in additional params, and call `getData()` function
    - `getData` function updated to update url value if passed in, call `getData` function of visualization passing appropriate options
    - `onDataLoaded` function added to conditionally store the table data and call `showLayout` function or call `ready` function
    - `ready` function added to make the gadgets rpc call of "rsevent_ready"

--- src/widget/spreadsheet.js
+++ src/widget/spreadsheet.js
@@ -3,7 +3,7 @@
 var RiseVision = RiseVision || {};
 RiseVision.Spreadsheet = {};
 
-RiseVision.Spreadsheet = (function (window, document, gadgets, utils) {
+RiseVision.Spreadsheet = (function (window, document, gadgets, utils, Visualization) {
 
   "use strict";
 
@@ -12,37 +12,13 @@ RiseVision.Spreadsheet = (function (window, document, gadgets, utils) {
     DATA_FONT_CLASS = "data_font-style";
 
   // private variables
-  var _url;
+  var _prefs = null,
+    _spreadsheetData = {},
+    _isLoading = true,
+    _vizData, _viz;
 
-  function _additionalParams(name, value) {
-    if (name === "additionalParams") {
-      if (value) {
-        var styleNode = document.createElement("style");
-
-        value = JSON.parse(value);
-
-        //Inject CSS fonts into the DOM since they are stored as additional parameters.
-        styleNode.appendChild(document.createTextNode(utils.getFontCssStyle(HEADING_FONT_CLASS, value.table.colHeaderFont)));
-        styleNode.appendChild(document.createTextNode(utils.getFontCssStyle(DATA_FONT_CLASS, value.table.dataFont)));
-        styleNode.appendChild(document.createTextNode("a:active" + utils.getFontCssStyle(DATA_FONT_CLASS, value.table.dataFont)));
-
-        console.log(styleNode);
-
-        document.getElementsByTagName("head")[0].appendChild(styleNode);
-
-      }
-    }
-
-    //TODO: more logic to come
-
-  }
-
-  function _getData(url) {
-    if (url) {
-      _url = url;
-    }
-
-    //TODO: continue logic from here
+  function _showLayout() {
+    //TODO: logic to come
   }
 
   function _pause() {
@@ -53,6 +29,70 @@ RiseVision.Spreadsheet = (function (window, document, gadgets, utils) {
     //TODO: logic to come
   }
 
+  function _ready() {
+    gadgets.rpc.call("", "rsevent_ready", null, _prefs.getString("id"), true, true, true, true, true);
+  }
+
+  function _onDataLoaded(data) {
+    if (!data) {
+      if (_isLoading) {
+        _isLoading = false;
+        _ready();
+      }
+    }
+    else {
+      _pause();
+      // store the table data from visualization
+      _vizData = data;
+
+      _showLayout();
+    }
+  }
+
+  function _getData(url) {
+    if (url) {
+      _spreadsheetData.url = url;
+    }
+
+    _viz.getData({
+      url: _spreadsheetData.url,
+      refreshInterval: parseInt(_spreadsheetData.refresh,10),
+      callback: _onDataLoaded
+    });
+  }
+
+  function _additionalParams(name, value) {
+    // create visualization instance
+    if (!_viz) {
+      _viz = new Visualization();
+    }
+
+    _prefs = new gadgets.Prefs();
+
+    if (name === "additionalParams") {
+      if (value) {
+        var styleNode = document.createElement("style");
+
+        value = JSON.parse(value);
+
+        // store the spreadsheet data that was saved in settings
+        _spreadsheetData = $.extend({}, value.spreadsheet);
+
+        //Inject CSS fonts into the DOM since they are stored as additional parameters.
+        styleNode.appendChild(document.createTextNode(utils.getFontCssStyle(HEADING_FONT_CLASS, value.table.colHeaderFont)));
+        styleNode.appendChild(document.createTextNode(utils.getFontCssStyle(DATA_FONT_CLASS, value.table.dataFont)));
+        styleNode.appendChild(document.createTextNode("a:active" + utils.getFontCssStyle(DATA_FONT_CLASS, value.table.dataFont)));
+
+        document.getElementsByTagName("head")[0].appendChild(styleNode);
+
+      }
+
+      _getData();
+
+      //TODO: more handling of CSS for a table layout
+    }
+  }
+
   return {
     additionalParams: _additionalParams,
     getData: _getData,
@@ -60,6 +100,6 @@ RiseVision.Spreadsheet = (function (window, document, gadgets, utils) {
     play: _play
   };
 
-})(window, document, gadgets, RiseVision.Common.Utilities);
+})(window, document, gadgets, RiseVision.Common.Utilities, RiseVision.Common.Visualization);
 
 
