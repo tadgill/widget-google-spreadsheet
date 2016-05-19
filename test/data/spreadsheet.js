e26853154b82eeba3a578e215d2d90ece31af21c
rodrigopavezi <rodrigopavezi@gmail.com>
2016-05-19 21:21:25 +0100

    Implemented first integration tests

--- test/data/spreadsheet.js
+++ test/data/spreadsheet.js
@@ -6,13 +6,58 @@
 
   window.gadget.settings = {
     "params": {},
-    "additionalParams": {
+    additionalParams: {
+      format: {
+        body: {
+          fontStyle:{
+            font:{
+              family:"verdana,geneva,sans-serif",
+              type:"standard",
+              url:""
+            },
+            size:"18px",
+            customSize:"",
+            align:"left",
+            bold:false,
+            italic:false,
+            underline:false,
+            forecolor:"black",
+            backcolor:"transparent"
+          }
+        },
+        evenRowColor: "rgb(246, 247, 248)",
+        header: {
+          fontStyle:{
+            font:{
+              family:"verdana,geneva,sans-serif",
+              type:"standard",
+              url:""
+            },
+            size:"18px",
+            customSize:"",
+            align:"left",
+            bold:false,
+            italic:false,
+            underline:false,
+            forecolor:"black",
+            backcolor:"transparent"
+          }
+        },
+        oddRowColor: "rgb(255, 255, 255)"
+      },
+      scroll: {},
       spreadsheet: {
+        selection: "drive",
+        docName: "",
+        url: "",
+        fileId: "",
         cells: "sheet",
         range: {
           startCell: "",
           endCell: ""
         },
+        tabId: 1,
+        hasHeader: true,
         refresh: 5
       }
     }
