0f883c453413384e4ffe406a2d7948a85eaa9d26
Stuart Lees <stuartlees@gmail.com>
2016-08-17 19:43:00 -0400

    Test coverage

--- test/mocks/google-sheet-http-mock.js
+++ test/mocks/google-sheet-http-mock.js
@@ -31,16 +31,48 @@
           ]
         };
 
-        function getHTTP(key) {
+        var successColumnsData = {
+          "range": "Worksheet 1!A1:F994",
+          "majorDimension": "COLUMNS",
+          "values": [
+            ["A1 value", "A2 value"],
+            ["B1 value", "B2 value"],
+            ["C1 value", "C2 value"],
+            ["D1 value", "D2 value"]
+          ]
+        };
+
+        var errorColumnsData = {
+          "error": {
+            "code": 400,
+            "message": "Unable to parse range: Cars!fsasaf:fdsfsd",
+            "status": "INVALID_ARGUMENT"
+          }
+        };
+
+        function getSpreadsheetHTTP(key) {
           return SHEETS_API + key + "?key=" + API_KEY;
         }
 
-        $httpBackend.whenGET(getHTTP("public")).respond(function () {
+        function getColumnsHTTP(range) {
+          return SHEETS_API + "public/values/Worksheet%201" + ((range) ? "!" + range : "") +
+            "?key=" + API_KEY + "&majorDimension=COLUMNS"
+        }
+
+        $httpBackend.whenGET(getSpreadsheetHTTP("public")).respond(function () {
           return [200, successData, {}];
         });
 
-        $httpBackend.whenGET(getHTTP("not-public")).respond(function () {
+        $httpBackend.whenGET(getSpreadsheetHTTP("not-public")).respond(function () {
           return [302, {}, {}];
+        });
+
+        $httpBackend.whenGET(getColumnsHTTP("")).respond(function () {
+          return [200, successColumnsData, {}];
+        });
+
+        $httpBackend.whenGET(getColumnsHTTP("abc:abc")).respond(function () {
+          return [400, errorColumnsData, {}];
         });
       }]);
 
--- test/unit/settings/svc-google-sheet-spec.js
+++ test/unit/settings/svc-google-sheet-spec.js
@@ -5,21 +5,6 @@ describe("getWorkSheets", function() {
 
   var googleSheetService, base, apiKey, httpBackend;
 
-  var successData = {
-    sheets: [
-      {
-        properties: {
-          title: "Worksheet 1"
-        }
-      },
-      {
-        properties: {
-          title: "Worksheet 2"
-        }
-      }
-    ]
-  };
-
   beforeEach(module("risevision.widget.googleSpreadsheet.settings"));
 
   beforeEach(inject(function (_googleSheet_, _SHEETS_API_, _API_KEY_, $httpBackend) {
@@ -34,33 +19,142 @@ describe("getWorkSheets", function() {
     expect(googleSheetService).be.defined;
   });
 
-  it("should successfully provide work sheets as select field options", function () {
+  describe("getWorkSheets", function () {
+    var successData = {
+      sheets: [
+        {
+          properties: {
+            title: "Worksheet 1"
+          }
+        },
+        {
+          properties: {
+            title: "Worksheet 2"
+          }
+        }
+      ]
+    };
+
     function getHTTP(key) {
       return base + key + "?key=" + apiKey;
     }
 
-    httpBackend.whenGET(getHTTP("public")).respond(function () {
-      return [200, successData, {}];
-    });
-
-    googleSheetService.getWorkSheets("public").then(function (results) {
-      expect(results).be.defined;
-      expect(results.length).to.equal(2);
-
-      expect(results[0]).to.deep.equal({
-        "label": "Worksheet 1",
-        "value": 1
+    it("should successfully provide work sheets as select field options", function () {
+      httpBackend.whenGET(getHTTP("public")).respond(function () {
+        return [200, successData, {}];
       });
 
-      expect(results[1]).to.deep.equal({
-        "label": "Worksheet 2",
-        "value": 2
+      googleSheetService.getWorkSheets("public").then(function (results) {
+        expect(results).be.defined;
+        expect(results.length).to.equal(2);
+
+        expect(results[0]).to.deep.equal({
+          "label": "Worksheet 1",
+          "value": 1
+        });
+
+        expect(results[1]).to.deep.equal({
+          "label": "Worksheet 2",
+          "value": 2
+        });
+
       });
 
+      httpBackend.flush();
     });
-
-    httpBackend.flush();
   });
 
+  describe("getColumnsData", function () {
+    var successColumnsData = {
+      "range": "Worksheet 1!A1:F994",
+      "majorDimension": "COLUMNS",
+      "values": [
+        ["A1 value", "A2 value"],
+        ["B1 value", "B2 value"],
+        ["C1 value", "C2 value"],
+        ["D1 value", "D2 value"]
+      ]
+    },
+      successColumnsRangeData = {
+        "range": "Worksheet 1!B1:C2",
+        "majorDimension": "COLUMNS",
+        "values": [
+          ["B1 value", "B2 value"],
+          ["C1 value", "C2 value"]
+        ]
+      },
+      errorResponse = {
+        "error": {
+          "code": 400,
+          "message": "Unable to parse range: Cars!fsasaf:fdsfsd",
+          "status": "INVALID_ARGUMENT"
+        }
+      };
+
+    function getHTTP(range) {
+      return base + "public/values/Worksheet%201" + ((range) ? "!" + range : "") +
+        "?key=" + apiKey + "&majorDimension=COLUMNS"
+    }
+
+    it("should provide column data", function () {
+      httpBackend.whenGET(getHTTP()).respond(function () {
+        return [200, successColumnsData, {}];
+      });
+
+      googleSheetService.getColumnsData("public", apiKey, "Worksheet 1")
+        .then(function (columns) {
+          expect(columns).be.defined;
+          expect(columns.length).to.equal(4);
+
+          expect(columns[0]).to.have.property("id");
+          expect(columns[0].id).to.equal(0);
+          expect(columns[0]).to.have.property("name");
+          expect(columns[0].name).to.equal("A");
+
+          // test to ensure null is provided when request same as previous successful request
+          googleSheetService.getColumnsData("public", apiKey, "Worksheet 1")
+            .then(function (columns) {
+              expect(columns).to.be.null;
+            });
+
+        });
+
+      httpBackend.flush();
+
+    });
+
+    it("should provide column data based on range", function () {
+      httpBackend.whenGET(getHTTP("B1:C2")).respond(function () {
+        return [200, successColumnsRangeData, {}];
+      });
+
+      googleSheetService.getColumnsData("public", apiKey, "Worksheet 1", "B1:C2").then(function (columns) {
+        expect(columns).be.defined;
+        expect(columns.length).to.equal(2);
+
+        expect(columns[0].id).to.equal(0);
+        expect(columns[0].name).to.equal("B");
+
+        expect(columns[1].id).to.equal(1);
+        expect(columns[1].name).to.equal("C");
+      });
+
+      httpBackend.flush();
+    });
+
+    it("should provide an error", function () {
+      httpBackend.whenGET(getHTTP()).respond(function () {
+        return [400, errorResponse, {}];
+      });
+
+      googleSheetService.getColumnsData("public", apiKey, "Worksheet 1")
+        .then(null, function (error) {
+          expect(error).be.defined;
+          expect(error).to.deep.equal(errorResponse.error);
+        });
+
+      httpBackend.flush();
+    });
+  });
 
 });
